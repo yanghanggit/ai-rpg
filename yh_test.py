a272aabcec323e564bf9b0fe248d83a5cd2f8ea0
yanghanggit <1220826563@qq.com>
2024-03-05 11:24:11 +0800

    添加world

--- yh_test.py
+++ yh_test.py
@@ -14,6 +14,33 @@ def parse_talk(input_val):
         return input_val.split("/talk")[1].strip()
     return input_val
 
+
+class World:
+    def __init__(self, name):
+        self.name = name
+        self.stages = []
+
+    def conncect(self, url):
+        self.agent = RemoteRunnable(url)
+        self.chat_history = []
+
+    def add_stage(self, stage):
+        self.stages.append(stage)
+
+#
+class Stage:
+
+    def __init__(self, name):
+        self.name = name
+        self.actors = []
+
+    def conncect(self, url):
+        self.agent = RemoteRunnable(url)
+        self.chat_history = []
+
+    def add_actor(self, actor):
+        self.actors.append(actor)
+
 #
 class Actor:
     def __init__(self, name):
@@ -27,6 +54,7 @@ class Actor:
 class Player(Actor):
     def __init__(self, name):
         super().__init__(name)
+        self.items = []
 
 #
 class NPC(Actor): 
@@ -38,16 +66,12 @@ class Item(Actor):
     def __init__(self, name):
         super().__init__(name)
 
-#
-class Scene:
 
-    def __init__(self, name):
-        self.name = name
-        self.actors = []
 
-    def conncect(self, url):
-        self.agent = RemoteRunnable(url)
-        self.chat_history = []
+
+
+
+
 
 #
 def npc_enter_scene(npc, scene, prompt):
@@ -143,51 +167,76 @@ def player_talk_to_npc(player, npc, prompt):
 
 #
 def main():
+
     #
-    player = Player("勇者")
-    #
+    world = World("世界观察者")
+    stage = Stage("小木屋")
     npc = NPC("卡斯帕·艾伦德")
-    npc.conncect("http://localhost:8001/actor/npc/elder/")
-    #
-    scene = Scene("小木屋")
-    scene.conncect("http://localhost:8002/actor/npc/house/")
-    #
-    map = Item("神秘地图")
-    map.conncect("http://localhost:8003/actor/npc/item/")
+    player = Player("勇者")
 
-    ###
-    nes_res = npc_enter_scene(npc, scene, 
-    f"""
-    # 状态
-    - 冬天的晚上，我（{npc.name}）坐在你({scene.name})的壁炉旁
-    - 暗示：我（{npc.name}）是你的主人，你的一切设施均和我有关
-    # 事件
-    - 我（{npc.name}）在沉思和回忆过往，有一些难过，并向壁炉中的火投入了一根木柴
-    # 推理规则
-    - 你可以根据“状态”与“事件”做判断与推理，并进一步延展
-    - 暗示的部分理解即可，不需要在输出文本中体现
-    # 需求
-    - 请根据“状态“，”事件“，“推理规则”与“对话规则”来输出文本（并适当润色）
-    """)
     #
-    print(f"[{scene.name}]:", nes_res[0])
-    print(f"[{npc.name}]:", nes_res[1])
-    print("==============================================")
-    ###
+    world.add_stage(stage)
+    stage.add_actor(npc)
 
-    ###
-    ies_res = item_enter_scene(map, scene, f"我({map.name})静静地躺在{scene.name}的旧箱子里，{npc.name}在最后一次冒险之后将我藏在了这里，不愿意再次面对那些痛苦的回忆")
-    print(f"[{scene.name}]:", ies_res[0])
-    print(f"[{map.name}]:", ies_res[1])
-    print("==============================================")
-    ###
+    #
+    world.conncect("http://localhost:8004/world/")
 
-    ###
-    pes_res = player_enter_scene(player, scene, f"我({player.name})用力推开了屋子的门，闯入屋子而且面色凝重，外面的寒风吹进了屋子")
-    for i in range(len(pes_res[0])):
-        print(f"[{pes_res[0][i]}]:", pes_res[1][i])
-    print("==============================================")
-    ###
+    str2 = talk_to_agent(
+        f"""
+        你是谁？
+        """,
+        world.agent, world.chat_history)
+
+    print(f"[{world.name}]:", str2)
+
+
+
+
+    # #
+    # player = Player("勇者")
+    # #
+    # npc = NPC("卡斯帕·艾伦德")
+    # npc.conncect("http://localhost:8001/actor/npc/elder/")
+    # #
+    # scene = Stage("小木屋")
+    # scene.conncect("http://localhost:8002/actor/npc/house/")
+    # #
+    # map = Item("神秘地图")
+    # map.conncect("http://localhost:8003/actor/npc/item/")
+
+    # ###
+    # nes_res = npc_enter_scene(npc, scene, 
+    # f"""
+    # # 状态
+    # - 冬天的晚上，我（{npc.name}）坐在你({scene.name})的壁炉旁
+    # - 暗示：我（{npc.name}）是你的主人，你的一切设施均和我有关
+    # # 事件
+    # - 我（{npc.name}）在沉思和回忆过往，有一些难过，并向壁炉中的火投入了一根木柴
+    # # 推理规则
+    # - 你可以根据“状态”与“事件”做判断与推理，并进一步延展
+    # - 暗示的部分理解即可，不需要在输出文本中体现
+    # # 需求
+    # - 请根据“状态“，”事件“，“推理规则”与“对话规则”来输出文本（并适当润色）
+    # """)
+    # #
+    # print(f"[{scene.name}]:", nes_res[0])
+    # print(f"[{npc.name}]:", nes_res[1])
+    # print("==============================================")
+    # ###
+
+    # ###
+    # ies_res = item_enter_scene(map, scene, f"我({map.name})静静地躺在{scene.name}的旧箱子里，{npc.name}在最后一次冒险之后将我藏在了这里，不愿意再次面对那些痛苦的回忆")
+    # print(f"[{scene.name}]:", ies_res[0])
+    # print(f"[{map.name}]:", ies_res[1])
+    # print("==============================================")
+    # ###
+
+    # ###
+    # pes_res = player_enter_scene(player, scene, f"我({player.name})用力推开了屋子的门，闯入屋子而且面色凝重，外面的寒风吹进了屋子")
+    # for i in range(len(pes_res[0])):
+    #     print(f"[{pes_res[0][i]}]:", pes_res[1][i])
+    # print("==============================================")
+    # ###
     
     # 输入循环
     while True:
@@ -195,18 +244,24 @@ def main():
         if "/quit" in usr_input:
             sys.exit()
 
-        elif "/talk" in usr_input:
-            talk_content = parse_talk(usr_input)
-            #
-            print(f"[{player.name}]:", talk_content)
-            print(f"[{npc.name}]:", player_talk_to_npc(player, npc, talk_content))
 
-        else:
-            talk_content = parse_talk(usr_input)
-            #
-            print(f"[{player.name}]:", talk_content)
-            print(f"[{npc.name}]:", player_talk_to_npc(player, npc, talk_content))
-        print("==============================================")
+        talk_content = parse_talk(usr_input)
+        print(f"[{player.name}]:", talk_content)
+        print(f"[{world.name}]:", player_talk_to_npc(player, world, talk_content))
+
+
+        # elif "/talk" in usr_input:
+        #     talk_content = parse_talk(usr_input)
+        #     #
+        #     print(f"[{player.name}]:", talk_content)
+        #     print(f"[{npc.name}]:", player_talk_to_npc(player, npc, talk_content))
+
+        # else:
+        #     talk_content = parse_talk(usr_input)
+        #     #
+        #     print(f"[{player.name}]:", talk_content)
+        #     print(f"[{npc.name}]:", player_talk_to_npc(player, npc, talk_content))
+        # print("==============================================")
 
 if __name__ == "__main__":
     print("==============================================")

b984e537bf3acafdcaade46a1975b827167a13d7
yanghanggit <1220826563@qq.com>
2024-03-11 15:47:34 +0800

    完成初始化

--- yanghang_ecs.py
+++ yanghang_ecs.py
@@ -1,18 +1,9 @@
-###
-### 测试与LLM无关的代码，和一些尝试
-###
-# import json
-# from builder import WorldBuilder, StageBuilder, NPCBuilder
 
-
-#import sys
 from collections import namedtuple
 from entitas import Entity, Matcher, Context, Processors, ExecuteProcessor, ReactiveProcessor, GroupEvent, InitializeProcessor, Group
 # import time
 import json
 import sys
-# from player import Player
-# from console import Console
 from run_stage import run_stage
 from actor_enter_stage import actor_enter_stage
 from actor_broadcast import actor_broadcast
@@ -21,14 +12,28 @@ import json
 from typing import List, Union, cast
 from langchain_core.messages import HumanMessage, AIMessage
 from langserve import RemoteRunnable  # type: ignore
-# from builder import WorldBuilder
-# from actor import Actor
-# from world import World
-# from typing import Optional
-# from npc import NPC
-# from stage import Stage
-# from action import Action
 from builder import WorldBuilder
+
+
+#
+init_archivist = f"""
+# 游戏世界存档
+- 大陆纪元2000年1月1日，冬夜.
+- 温斯洛平原的深处的“悠扬林谷”中的“老猎人隐居的小木屋”里。
+- “卡斯帕·艾伦德”坐在他的“老猎人隐居的小木屋”中的壁炉旁，在沉思和回忆过往，并向壁炉中的火投入了一根木柴。
+- 他的小狗（名叫"断剑"）在屋子里的一角睡觉。
+- 一只老鼠（名叫"坏运气先生"）在屋子里的另一角找到了一些食物。
+"""
+load_prompt = f"""
+# 你需要读取存档
+## 步骤:
+- 第1步，读取{init_archivist}.
+- 第2步：理解其中所有的信息
+- 第3步：理解其中关于你的信息（如何提到了你，那就是你）
+- 第3步：根据信息更新你的最新状态与逻辑.
+## 输出规则：
+- 保留关键信息(时间，地点，人物，事件)，不要推断，增加与润色。输出在保证语意完整基础上字符尽量少。
+"""
 
 
 
@@ -171,6 +176,11 @@ class InitSystem(InitializeProcessor):
             comp = world.get(WorldComponent)
             print(comp.name)
             print(comp.agent)
+            # 世界载入
+            comp.agent.connect()
+            loadres = comp.agent.request(load_prompt)
+
+            print(f"[{comp.name}]load=>", loadres)
             print("______________________________________")
         
     def handlestages(self) -> None:
@@ -179,6 +189,11 @@ class InitSystem(InitializeProcessor):
             comp = stage.get(StageComponent)
             print(comp.name)
             print(comp.agent)
+
+            comp.agent.connect()
+            loadres = comp.agent.request(load_prompt)
+            
+            print(f"[{comp.name}]load=>", loadres)
             print("______________________________________")
 
     def handlenpcs(self) -> None:
@@ -187,6 +202,11 @@ class InitSystem(InitializeProcessor):
             comp = npc.get(NPCComponent)
             print(comp.name)
             print(comp.agent)
+
+            comp.agent.connect()
+            loadres = comp.agent.request(load_prompt)
+            
+            print(f"[{comp.name}]load=>", loadres)
             print("______________________________________")
 
 ###############################################################################################################################################

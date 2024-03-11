3585dee8402440af0d1395977608b14704ee48b4
yanghanggit <1220826563@qq.com>
2024-03-11 18:55:33 +0800

    测试导演类

--- yanghang_ecs.py
+++ yanghang_ecs.py
@@ -446,19 +446,63 @@ class DirectorSystem(ExecuteProcessor):
         entities = self.context.get_group(Matcher(StageComponent)).entities
         for entity in entities:
             self.handle(entity)
-
             #清空
             comp = entity.get(StageComponent)
-            comp.events.clear()
-            print("000000000000000")
+            comp.events.clear() #不能 = []，会报错！！！
 
     def handle(self, entity: Entity) -> None:
         comp = entity.get(StageComponent)
-        print(f"[{comp.name}] events:")
+        print(f"[{comp.name}] 开始导演+++++++++++++++++++++++++++++++++++++++++++++++++++++")
         if len(comp.events) == 0:
             return
+    
+        #debug
         for event in comp.events:
             print("moive:", event)
+
+        allevents = "\n".join(comp.events)
+        director_prompt =  f"""
+        # 你按着我的给你的脚本来演绎过程，并适当润色让过程更加生动。
+        ## 剧本如下
+        - {allevents}
+        ## 步骤
+        - 第1步：理解我的剧本
+        - 第2步：根据剧本，完善你的故事讲述(同一个人物的行为描述要合并处理)。要保证和脚本的结果一致。
+        - 第3步：更新你的记忆
+        ## 输出规则
+        - 输出在保证语意完整基础上字符尽量少。
+        """
+        #
+        response = comp.agent.request(director_prompt)
+        print(f"{comp.name}=>", response)
+
+
+        npccomps = self.getnpcs(comp.name)
+        all_names = "、".join([ncomp.name for ncomp in npccomps])
+
+        confirm_prompt = f"""
+        # 你目睹或者参与了这一切，并更新了你的记忆
+        - {response}
+        # 你能确认
+        - {all_names} 都还存在。
+        """
+
+        for ncomp in npccomps:
+            response = ncomp.agent.request(confirm_prompt)
+            print(f"[{ncomp.name}]=>", response)
+
+
+        print(f"[{comp.name}] 结束导演+++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+
+    def getnpcs(self, stage: str) -> List[NPCComponent]:
+        npcs = []
+        for entity in self.context.get_group(Matcher(NPCComponent)).entities:
+            if entity.get(NPCComponent).current_stage == stage:
+                npcs.append(entity.get(NPCComponent))
+        return npcs
+
+
 ###############################################################################################################################################
 ###############################################################################################################################################
 ###############################################################################################################################################
@@ -483,19 +527,7 @@ def main() -> None:
 
     except Exception as e:
         print(e)
-        return    
-
-    # #StageComponent = namedtuple('StageComponent', 'name agent events')
-
-    # entities = context.get_group(Matcher(StageComponent)).entities
-    # for entity in entities:
-    #     comp = entity.get(StageComponent)
-    #     print(comp.name)
-    #     print(comp.agent)
-    #     print(comp.events)
-    #     comp.events.clear()
-    #     print("????????????")
-    
+        return        
 
     print("<<<<<<<<<<<<<<<<<<<<< 构建系统 >>>>>>>>>>>>>>>>>>>>>>>>>>>")
     #初始化系统

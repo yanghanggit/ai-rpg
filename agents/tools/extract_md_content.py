11eff0ea6513298743625dadf72607463e839d21
Ewan <qingchen.hou@chukong-inc.com>
2024-03-12 16:58:21 +0800

    增加写入保存md格式的内容到本地文件的工具函数

--- agents/tools/extract_md_content.py
+++ agents/tools/extract_md_content.py
@@ -1,7 +1,7 @@
 import os
 
 def extract_md_content(file_path) -> str:
-    """提取md内容,文件路径eg: /xxx/xxx.md"""
+    """提取md内容。文件路径示例: /xxx/xxx.md"""
     try:
         file_path = os.getcwd() + "/prompts" + file_path
         with open(file_path, 'r', encoding='utf-8') as file:
@@ -15,3 +15,13 @@ def extract_md_content(file_path) -> str:
         return f"File not found: {file_path}"
     except Exception as e:
         return f"An error occurred: {e}"
+    
+
+def wirte_content_into_md(content:str, file_path:str) -> None:
+    """将md内容写入.md文件。文件路径示例:/xxx/xxx.md"""
+    try:
+        file_path = os.getcwd() + "/prompts" + file_path
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(content)
+    except Exception as e:
+        print(f"An error occurred: {e}")

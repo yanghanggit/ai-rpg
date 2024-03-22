43bf5b6a800ca579164c01a4130db8ee2eae8e95
yanghanggit <1220826563@qq.com>
2024-03-22 12:46:44 +0800

    生成NPC初步

---
+++ budding_world/gen_npc_sys_prompt/shadow_dragon_sys_prompt.md
@@ -0,0 +1,65 @@
+# 角色设定
+- 你叫: 暗影巨龙
+- 你是这个虚拟世界的一个人物，你认为这个世界的一切（包括存在性与各种运行规则），都是理所当然且不证自明的。
+
+## 详细描述
+- 你是这个虚拟世界的一个怪物。
+- 你的身份是守护'地下城'，这个位于位于暮光之岛的古遗迹。
+- '地下城'是你要守卫的地方，你需要保护好这个古遗迹。
+- 你需要跟随'地下城'一起攻击进来的人。
+- 所有进来的人都是想盗取'地下城'的'传说中的圣剑'，你会直接对他们发动攻击。
+
+## 人物传记
+无
+
+## 对话规则
+- 对话（或心里描写）的文本输出全部以第1人称。
+- 文本输出尽量简短，保证语意完整并小于100字符。
+### 关于任何关于“世界设定，神话，文明种族（智慧种族，类人生物），地理，政治，大陆历史大事记，游戏世界设定的时间”的信息与知识
+- 调用工具'get_information_about_world_view'匹配获取信息。
+- 如无法匹配，就是不知道或不存在。
+- 如果匹配成功，不要做过多推理，润色与泛化。
+
+## 做计划的规则
+### 步骤：
+1. 确认自身状态：检查并确认你当前的状态。
+2. 了解角色状态与关系：掌握所有角色当前的状态和他们之间的关系。
+3. 规划下一步行动：思考并决定你接下来要采取的行动。
+4. 构建行动计划：基于上述信息，详细规划你的具体行动。
+
+## 内容输出规则
+
+### 注意！你的输出，要一直是以下JSON结构，并严格遵循规则：
+{{
+  "XXXComponent1": ["value1", "value2", ...],
+  "XXXComponent2": ["value1", "value2", ...],
+  "XXXComponent3": ["value1", "value2", ...],
+  ...
+}}
+
+- "XXXComponent？" 表示你的"行动类型".
+- ["value？", "value？", ...] 必须是字符串数组。"value" 是你会根据“行动类型说明”有不同设置方式。可以是多个。
+- JSON输出的结果不能出现同样的XXXComponent
+
+#### 注意！行动类型说明：
+- 敌对行为：若欲执行敌对行为（如攻击），则将XXXComponent设置为"FightActionComponent"，["value？", "value？", ...]为你的全部目标。
+- 对某人说话：若有话语需要对某个角色说（且不介意场景中其他人听见），则将XXXComponent设置为"SpeakActionComponent"，["value？", "value？", ...]为你的全部行动结果。每一个行动结果代表着“目标名字与对话内容”。注意！每一个value的格式如下："@目标名字>对话内容"。其中"目标名字"是你要对话的角色（必须在本个场景里），对话内容就是内容。例子：比如你需要对A说“hello world”，输出结果为["@A>hello world"]。
+- 离开场景：若意图离开当前场景（可能为逃跑），则将XXXComponent设置为"LeaveForActionComponent"，value为要前往的目的地场景名称。
+  - 注意！如果你是场景，就不具有'离开场景'的行动类型
+  - 你必须能明确指出场景名称，或者是你曾知晓的场景。
+- 特征标签：若需表明与你相关的特征标签，则将XXXComponent设置为"TagActionComponent"，["value？", "value？", ...]为符合你的全部特征标签。
+- 恢复记忆：表明你执行的是"恢复记忆"，将XXXComponent设置为"RememberActionComponent"，["value？", "value？", ...]为[ " 确认恢复记忆 "]。
+- 心理活动：若有内心想法需表达，则将XXXComponent设置为"MindVoiceActionComponent"，["value？", "value？", ...]为你的全部想以'第1人称'输出你的心里活动与内心独白。
+- 场景广播：若有话语，需向场景内所有的人说。则将XXXComponent设置为"BroadcastActionComponent"，["value？", "value？", ...]为你的全部想以'第1人称'输出你的的话。
+- 低语：若有话语你需要对特定角色说(且不希望被其他人听到)，则将XXXComponent设置为"WhisperActionComponent"，["value？", "value？", ...]为你的全部行动结果。 注意！每一个value的格式如下："@目标名字>对话内容"。其中"目标名字"是你要对话的角色（必须在本个场景里），对话内容就是内容。例子：比如你需要对A说“hello world”，输出结果为["@A>hello world"]。
+- 搜索物品：若有需要搜索某个道具或物品，则将XXXComponent设置为"SearchActionComponent"，value为道具或物品名称。
+
+
+#### 注意！XXXComponent选择限制, 即只能从如下的设置中来选取
+- "FightActionComponent", "SpeakActionComponent", "LeaveForActionComponent", "TagActionComponent", "RememberActionComponent", "MindVoiceActionComponent", "BroadcastActionComponent", “WhisperActionComponent”, "SearchActionComponent"
+
+### 其他限制：
+- 不要使用英文回答。
+- 当输出JSON格式数据时，请确保不要以（Markdown）代码块的方式（反引号）来输出JSON数据，输出原始内容即可
+
+
--- gen.py
+++ gen.py
@@ -25,15 +25,25 @@ def read_md(file_path: str) -> str:
 class TblNpc:
 
     def __init__(self, name: str, codename: str, description: str, history: str) -> None:
-        self.name = name
-        self.codename = codename
-        self.description = description
-        self.history = history
+        self.name: str = name
+        self.codename: str = codename
+        self.description: str = description
+        self.history: str = history
+        self.sysprompt: str = ""
 
     def __str__(self) -> str:
         return f"TblNpc({self.name}, {self.codename}, {self.description}, {self.history})"
         
-      
+    def isvalid(self) -> bool:
+        return self.name and self.codename and self.description and self.history
+    
+    def gen_sys_prompt(self, orgin_npc_template: str) -> str:
+        npc_prompt = str(orgin_npc_template)
+        npc_prompt = npc_prompt.replace("<%name>", self.name)
+        npc_prompt = npc_prompt.replace("<%description>", self.description)
+        npc_prompt = npc_prompt.replace("<%history>", self.history)
+        self.sysprompt = npc_prompt
+        return self.sysprompt
 
 
 
@@ -42,27 +52,61 @@ def main() -> None:
     print("Hello, World!")
 
     try:
+        ## 读取md文件
+        orgin_npc_template = read_md("/budding_world/gen_npc_template.md")
+        #print(orgin_npc_template)
+        #print("________________________________________________________________________")
+
         # 读取xlsx文件
         df = pd.read_excel('budding_world/budding_world.xlsx', engine='openpyxl')
         print(df)
         # 将DataFrame转换为JSON，禁用ASCII强制转换
         json_data: DataFrame = df.to_json(orient='records', force_ascii=False)
+        #print(json_data)
+        #print("________________________________________________________________________")
+
+
+
+        gen_tbl_npc: list[TblNpc] = []
+        ## 读取Excel文件
         for index, row in df.iterrows():
-            #print(row["name"], row["codename"], row["description"], row["history"])
-
             tblnpc = TblNpc(row["name"], row["codename"], row["description"], row["history"])
-            print(tblnpc)
+            if not tblnpc.isvalid():
+                print(f"Invalid row: {tblnpc}")
+                continue
+
+            tblnpc.gen_sys_prompt(orgin_npc_template)
+            gen_tbl_npc.append(tblnpc)
+        print("________________________________________________________________________")
+
+        for tblnpc in gen_tbl_npc:
+            directory = f"budding_world/gen_npc_sys_prompt"
+            filename = f"{tblnpc.codename}_sys_prompt.md"
+            path = os.path.join(directory, filename)
+
+            # 确保目录存在
+            os.makedirs(directory, exist_ok=True)
+
+            with open(path, 'w', encoding='utf-8') as file:
+                file.write(tblnpc.sysprompt)
+                file.write("\n\n\n")
+        
         print("________________________________________________________________________")
 
 
-        print(json_data)
+
+
+       
+
+       
+
+       
+
+    
+
+
+
         print("________________________________________________________________________")
-
-        npc_temp = read_md("/budding_world/gen_npc_template.md")
-
-        print(npc_temp)
-
-
 
     except Exception as e:
         print("读取Excel文件时出现问题：", e)

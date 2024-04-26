e0ce9ba9324d511c1c4bd2ed94e121e66e313d94
yanghanggit <1220826563@qq.com>
2024-04-26 17:49:33 +0800

    关闭一些暂时不用的prompt

--- auxiliary/prompt_maker.py
+++ auxiliary/prompt_maker.py
@@ -25,15 +25,15 @@ def read_archives_when_system_init_prompt(archives: str, entity: Entity, context
     """
     return prompt
 
-def confirm_everything_after_director_add_new_memories_prompt(allevents: list[str], npcs_names: str, stagename: str, context: ExtendedContext) -> str:
-    prompt = f"""
-    # 下面是已经发生的事情,你目睹或者参与了这一切，并更新了你的记忆,如果与你记忆不相符则按照下面内容强行更新你的记忆:
-    - {allevents}
-    # 你能确认
-    - {npcs_names} 都还在此 {stagename} 场景中。
-    - 你需要更新你的状态并以此作为做后续计划的基础。
-    """
-    return prompt
+# def confirm_everything_after_director_add_new_memories_prompt(allevents: list[str], npcs_names: str, stagename: str, context: ExtendedContext) -> str:
+#     prompt = f"""
+#     # 下面是已经发生的事情,你目睹或者参与了这一切，并更新了你的记忆,如果与你记忆不相符则按照下面内容强行更新你的记忆:
+#     - {allevents}
+#     # 你能确认
+#     - {npcs_names} 都还在此 {stagename} 场景中。
+#     - 你需要更新你的状态并以此作为做后续计划的基础。
+#     """
+#     return prompt
 
 def whisper_action_prompt(srcname: str, destname: str, content: str, context: ExtendedContext) -> str:
     prompt = f"{srcname}对{destname}低语道:{content}"   
@@ -120,11 +120,11 @@ def search_failed_prompt(npcname: str, prop_name:str) -> str:
 #     return ""
 
     
-def fail_to_enter_stage(npc_name: str, stage_name: str, enter_condition: str) -> str:
-    return f"{npc_name}试图进入{stage_name} 但背包中没有{enter_condition}，不能进入，或许{npc_name}需要尝试搜索一下'{enter_condition}'."
+# def fail_to_enter_stage(npc_name: str, stage_name: str, enter_condition: str) -> str:
+#     return f"{npc_name}试图进入{stage_name} 但背包中没有{enter_condition}，不能进入，或许{npc_name}需要尝试搜索一下'{enter_condition}'."
 
-def fail_to_exit_stage(npc_name: str, stage_name: str, exit_condition: str) -> str:
-    return f"{npc_name}试图离开{stage_name} 但背包中没有{exit_condition}，不能离开，或许{npc_name}需要尝试搜索一下'{exit_condition}'."
+# def fail_to_exit_stage(npc_name: str, stage_name: str, exit_condition: str) -> str:
+#     return f"{npc_name}试图离开{stage_name} 但背包中没有{exit_condition}，不能离开，或许{npc_name}需要尝试搜索一下'{exit_condition}'."
 
 def npc_enter_stage(npc_name: str, stage_name: str) -> str:
     return f"{npc_name}进入了{stage_name} 场景。"
@@ -139,8 +139,8 @@ def attack_someone(attacker_name: str, target_name: str, damage: int, target_cur
     health_percent = (target_current_hp - damage) / target_max_hp * 100
     return f"{attacker_name}对{target_name}发动了一次攻击,造成了{damage}点伤害,当前{target_name}的生命值剩余{health_percent}%。"
 
-def speak_action_system_invalid_target(target_name: str, speakcontent: str) -> str:
-    return f"[{target_name}]在你所在场景内无法找到，所以你不能对其说如下的内容：{speakcontent}"
+# def speak_action_system_invalid_target(target_name: str, speakcontent: str) -> str:
+#     return f"[{target_name}]在你所在场景内无法找到，所以你不能对其说如下的内容：{speakcontent}"
 
 def perception_action_prompt(npcnames: str, propnames: str) -> str:
     return f"你感知到了场景中有这些角色{npcnames}, 场景中有这些道具{propnames}"

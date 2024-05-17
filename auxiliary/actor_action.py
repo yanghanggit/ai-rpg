a711166b258bf51db79f3bc6989b0b2334c992cc
yanghanggit <1220826563@qq.com>
2024-05-17 12:39:59 +0800

    添加处理GPT4仍然有可能出现的json错误。md json 与 重复输出

--- auxiliary/actor_action.py
+++ auxiliary/actor_action.py
@@ -1,7 +1,83 @@
-from typing import List, Dict
+from typing import List, Dict, Any, Optional, cast
 import json
-from typing import Any
 from loguru import logger
+import re
+
+
+TEST_ERROR_REPEAT_JSON = f"""
+{{
+    "PerceptionActionComponent": ["灰颜墓地"],
+    "SpeakActionComponent": ["@哈哈哈哈哈>啦啦啦啦"],
+    "CheckStatusActionComponent": ["埃利亚斯·格雷"],
+    "TagActionComponent": ["守墓人", "阴郁", "畸形"]
+}}
+{{
+    "PerceptionActionComponent": ["灰颜墓地"],
+    "BroadcastActionComponent": ["哈哈哈哈哈"],
+    "CheckStatusActionComponent": ["埃利亚斯·格雷"]
+}}"""
+
+def merge_json(json_str: str) -> Optional[Dict[str, List[str]]]:
+    try:
+        #清理干净
+        _copy = str(json_str).strip()
+        _copy = _copy.replace('\n', '')
+        _copy = _copy.replace('\t', '')
+        _copy = re.sub(r'\s+', '', _copy)
+
+        #分割与重组
+        json_parts = re.split(r'}\s*{', _copy)
+        json_parts = [part + '}' if not part.endswith('}') else part for part in json_parts]
+        json_parts = ['{' + part if not part.startswith('{') else part for part in json_parts]
+
+        # 解析为JSON对象
+        json_objects = [json.loads(part) for part in json_parts]
+
+        # 合并这两个JSON对象
+        merged_json: Any = {}
+        for obj in json_objects:
+            for key, value in obj.items():
+                if key in merged_json:
+                    if not isinstance(merged_json[key], list):
+                        merged_json[key] = [merged_json[key]]
+                    merged_json[key].extend(value if isinstance(value, list) else [value])
+                else:
+                    merged_json[key] = value
+
+        # 将所有值转换为唯一列表
+        for key in merged_json:
+            if isinstance(merged_json[key], list):
+                merged_json[key] = list(set(merged_json[key]))
+
+        return cast(Dict[str, List[str]], merged_json) 
+           
+    except Exception as e:
+        logger.error(f"merge_json_strings failed. {e}")
+        return None
+    return None   
+
+## 检查是否是
+def is_repeat_error_json(errorjson: str) -> bool:
+    json_parts = re.split(r'}\s*{', errorjson)
+    return len(json_parts) > 1
+
+## 测试代码
+if is_repeat_error_json(TEST_ERROR_REPEAT_JSON):
+    jjj = merge_json(TEST_ERROR_REPEAT_JSON)
+    logger.debug(jjj)
+
+## 是否是
+def is_markdown_json_block(md_json_block: str) -> bool:
+    return "```json" in md_json_block
+
+## 处理
+def extract_markdown_json_block_content(jsonblock: str) -> str:
+    if "```json" in jsonblock:
+        copyvalue = str(jsonblock).strip()
+        copyvalue = copyvalue.replace("```json", "").replace("```", "").strip()
+        return copyvalue
+    
+    return jsonblock
 
 ############################################################################################################
 class ActorAction:
@@ -31,10 +107,32 @@ class ActorPlan:
         self.raw_data: str = raw_data
         self.json_data: Dict[str, List[str]] = {}
         self.actions: List[ActorAction] = []
-        self.json_content = self.check_markdown_json_block_and_handle(self.raw_data)
+
+        # 以防万一的检查
+        self.json_content = self.handle_md_json_block_and_repeat_content(self.raw_data)
 
         #核心执行
         self.load_then_build() 
+
+############################################################################################################
+    def handle_md_json_block_and_repeat_content(self, raw_data: str) -> str:
+        # step0: 不要改以前的。
+        result = str(raw_data)
+
+        #step1: GPT4 也有可能输出markdown json block。以防万一，我们检查一下。
+        if is_markdown_json_block(result):
+            logger.error(f"ActorPlan: has markdown json block {result}")
+            result = extract_markdown_json_block_content(result)
+
+        #step2: GPT4 也有可能输出重复的json。我们合并一下。有可能上面的json block的错误也犯了，所以放到第二个步骤来做
+        if is_repeat_error_json(result):
+            logger.error(f"ActorPlan: has repeat json {result}")
+            merge_res = merge_json(result)
+            if merge_res is not None:
+                result = json.dumps(merge_res)
+
+        # 有可能什么都不做，也有可能出现了错误进行了处理
+        return result
 ############################################################################################################
     def load_then_build(self) -> None:
         try:
@@ -54,16 +152,6 @@ class ActorPlan:
         for key, value in json.items():
             action = ActorAction(self.name, key, value)
             self.actions.append(action)
-############################################################################################################
-    #gpt4 我也发现会出现这种情况。我会尝试解决这个问题。
-    def check_markdown_json_block_and_handle(self, md_json_block: str) -> str:
-        if "```json" in md_json_block:
-            logger.error(f"ActorPlan: {self.name} has markdown json block {md_json_block}")
-            copyvalue = str(md_json_block).strip()
-            copyvalue = copyvalue.replace("```json", "").replace("```", "").strip()
-            return copyvalue
-        
-        return md_json_block
 ############################################################################################################
     def check_data_format(self, json_data: Any) -> bool:
         for key, value in json_data.items():

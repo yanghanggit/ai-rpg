93b53d0ba766076345d5424d14e99795fb98f65a
yanghanggit <1220826563@qq.com>
2024-06-01 19:07:24 +0800

    提交代码。临时解决违反政策的player 的对话输入问题

--- systems/post_conversation_action_system.py
+++ systems/post_conversation_action_system.py
@@ -1,33 +1,14 @@
 from entitas import Entity, Matcher, ReactiveProcessor, GroupEvent # type: ignore
-from auxiliary.components import SpeakActionComponent, BroadcastActionComponent, WhisperActionComponent, PlayerComponent
-from auxiliary.actor_action import ActorAction
+from auxiliary.components import SpeakActionComponent, BroadcastActionComponent, WhisperActionComponent, PlayerComponent, NPCComponent
 from auxiliary.extended_context import ExtendedContext
 from loguru import logger
-from auxiliary.dialogue_rule import dialogue_enable, parse_target_and_message, ErrorDialogueEnable
-from auxiliary.director_component import notify_stage_director
-from auxiliary.director_event import IDirectorEvent
-from typing import Optional
-from auxiliary.cn_builtin_prompt import speak_action_prompt
+from auxiliary.director_component import StageDirectorComponent
+from typing import List
+from auxiliary.agent_connect_system import AgentConnectSystem
 
-#post_conversation_action_system
-
-# ####################################################################################################################################
-# ####################################################################################################################################
-# ####################################################################################################################################
-# class SpeakEvent(IDirectorEvent):
-
-#     def __init__(self, who_is_speaking: str, who_is_target: str, message: str) -> None:
-#         self.who_is_speaking = who_is_speaking
-#         self.who_is_target = who_is_target
-#         self.message = message
-
-#     def tonpc(self, npcname: str, extended_context: ExtendedContext) -> str:
-#         speakcontent: str = speak_action_prompt(self.who_is_speaking, self.who_is_target, self.message, extended_context)
-#         return speakcontent
-    
-#     def tostage(self, stagename: str, extended_context: ExtendedContext) -> str:
-#         speakcontent: str = speak_action_prompt(self.who_is_speaking, self.who_is_target, self.message, extended_context)
-#         return speakcontent
+# todo 如果测试没问题，最好改成并发的
+# 这个类就是故意打包将对话类事件先进行一次request，如果LLM发现有政策问题就会抛异常，不会将污染的message加入chat history，这样就不可能进入chat_history。
+# 这么做是防止玩家的对话内容包含了非法信息和违反政策的内容。
 ####################################################################################################
 class PostConversationActionSystem(ReactiveProcessor):
     def __init__(self, context: ExtendedContext) -> None:
@@ -35,31 +16,69 @@ class PostConversationActionSystem(ReactiveProcessor):
         self.context = context
 ####################################################################################################
     def get_trigger(self) -> dict[Matcher, GroupEvent]:
-        return {Matcher( any_of=[SpeakActionComponent, BroadcastActionComponent, WhisperActionComponent]): GroupEvent.ADDED}
+        return {Matcher(any_of=[SpeakActionComponent, BroadcastActionComponent, WhisperActionComponent]): GroupEvent.ADDED}
 ####################################################################################################
     def filter(self, entity: Entity) -> bool:
-        return entity.has(PlayerComponent)
+        return entity.has(PlayerComponent) and entity.has(NPCComponent)
 ####################################################################################################
     def react(self, entities: list[Entity]) -> None:
-        pass
-        # for entity in entities:
-        #     self.speak(entity)  
+        for entity in entities:
+            self.before_handle(entity)
+            self.handle(entity)
+            self.after_handle(entity)
+####################################################################################################   
+    def before_handle(self, entity: Entity) -> None:
+        logger.warning(f"这里可以写，如果你说出来的话包涵了你目前不该知道的信息，就可以关掉。例如用broadcast喊某个地点，然后就能去了之类的")
 ####################################################################################################
-    # def speak(self, entity: Entity) -> None:
-    #     speakcomp: SpeakActionComponent = entity.get(SpeakActionComponent)
-    #     speakaction: ActorAction = speakcomp.action
-    #     safe_npc_name = self.context.safe_get_entity_name(entity)
-    #     for value in speakaction.values:
+    def after_handle(self, entity: Entity) -> None:
+        stage_entity = self.context.safe_get_stage_entity(entity)
+        if stage_entity is None:
+            return
+        stage_director_comp: StageDirectorComponent = stage_entity.get(StageDirectorComponent)
+        stage_director_comp.events.clear()
+####################################################################################################
+    def handle(self, entity: Entity) -> None:
+        stage_entity = self.context.safe_get_stage_entity(entity)
+        if stage_entity is None:
+            return
+        stage_director_comp: StageDirectorComponent = stage_entity.get(StageDirectorComponent)
+        if len(stage_director_comp.events) == 0:
+            return
+        ##
+        agent_connect_system: AgentConnectSystem = self.context.agent_connect_system
+        #
+        events2stage = stage_director_comp.tostage(stage_director_comp.name, self.context) 
+        batch_events2stage = self.batch_stage_events(events2stage) 
+        try:
+            response = agent_connect_system.request(stage_director_comp.name, batch_events2stage)
+            if response is None:
+                logger.error(f"handle: {stage_director_comp.name} request error.")
+        except Exception as e:
+            logger.error(f"handle: {stage_director_comp.name} request error.")
 
-    #         parse = parse_target_and_message(value)
-    #         targetname: Optional[str] = parse[0]
-    #         message: Optional[str] = parse[1]
-            
-    #         if targetname is None or message is None:
-    #             continue
-    
-    #         if dialogue_enable(self.context, entity, targetname) != ErrorDialogueEnable.VALID:
-    #             continue
-
-    #         notify_stage_director(self.context, entity, SpeakEvent(safe_npc_name, targetname, message))
+        ### 处理NPC的
+        npcs_in_this_stage = self.context.npcs_in_this_stage(stage_director_comp.name)
+        for npc_entity in npcs_in_this_stage:
+            if npc_entity == entity:
+                continue
+            ###
+            npccomp: NPCComponent = npc_entity.get(NPCComponent)
+            events2npc = stage_director_comp.tonpc(npccomp.name, self.context)     
+            batch_events2npc = self.batch_npc_events(events2npc)
+            try:
+                response = agent_connect_system.request(npccomp.name, batch_events2npc)
+                if response is None:
+                    logger.error(f"handle: {npccomp.name} request error.")
+            except Exception as e:
+                logger.error(f"handle: {npccomp.name} request error.")
+####################################################################################################
+    def batch_stage_events(self, events2stage: List[str]) -> str:
+        joinstr: str = "\n".join(events2stage)
+        prompt = f""" # 当前场景发生了如下对话类型事件，请注意:\n{joinstr}"""
+        return prompt
+####################################################################################################
+    def batch_npc_events(self, events2npc: List[str]) -> str:
+        joinstr: str = "\n".join(events2npc)
+        prompt = f""" # 当前场景发生了如下对话类型事件，请注意:\n{joinstr}"""
+        return prompt
 ####################################################################################################

2e5adce49858c2361f10e297e065e1f03b68a23d
Ewan <qingchen.hou@chukong-inc.com>
2024-06-04 14:59:30 +0800

    * 增加一个debug日志等问题复现

--- auxiliary/agent_connect_system.py
+++ auxiliary/agent_connect_system.py
@@ -197,5 +197,7 @@ class AgentConnectSystem:
         last_message = chat_history[-1]
         if isinstance(last_message, AIMessage):
             assert content == last_message.content
+            # tofix：出现过assert响的情况，加这个log用来下次出现时定位用
+            logger.debug(f"content:{content} and last_message:{last_message.content}")
             chat_history.pop()
 ############################################################################################################

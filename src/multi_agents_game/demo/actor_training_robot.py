912227104537930bc3d928ff9aa9c2c00a0613cc
Hongyu Cai <145795296+TheBestDustman@users.noreply.github.com>
2025-08-26 17:30:23 +0800

    复原抽卡提示词，更改环境互动技能提示词位置

--- src/multi_agents_game/demo/actor_training_robot.py
+++ src/multi_agents_game/demo/actor_training_robot.py
@@ -23,6 +23,6 @@ def create_actor_training_robot() -> Actor:
         rpg_character_profile=RPGCharacterProfile(base_dexterity=1),
         type=ActorType.MONSTER,
         campaign_setting=FANTASY_WORLD_RPG_CAMPAIGN_SETTING,
-        actor_profile="""你是一个训练机器人，你不会动，不会使用和生成任何技能，但你是死不掉的，你有无限的血量，""",
-        appearance="""你其实长的和稻草人一模一样，但是比稻草人身上多了一些金属盔甲，你被绑在一根柱子上。""",
+        actor_profile="""你是一个训练机器人，只会最基本的防御和攻击，你不会生成观察和利用环境的技能，但你是死不掉的，你有无限的血量，""",
+        appearance="""你长的和稻草人一模一样，但是身上多了一些金属盔甲，你被绑在一根柱子上,手上只握着一根木棍。""",
     )
--- src/multi_agents_game/demo/stage_dungeon5.py
+++ src/multi_agents_game/demo/stage_dungeon5.py
@@ -19,7 +19,7 @@ def create_stage_cave5() -> Stage:
         kick_off_message="",
         campaign_setting=FANTASY_WORLD_RPG_CAMPAIGN_SETTING,
         type=StageType.DUNGEON,
-        stage_profile="你是一个用来测试技能和战斗策略的训练场，训练场里有各种可以利用的环境物体。",
+        stage_profile="你是一个用来测试技能和战斗策略的训练场，训练场里只有干草和木桩。",
         actors=[],
     )
 
@@ -28,7 +28,7 @@ def create_demo_dungeon5() -> Dungeon:
 
     actor_training_robot = create_actor_training_robot()
     actor_training_robot.rpg_character_profile.hp = 1
-    actor_training_robot.kick_off_message += f"""\n注意：你作为一个训练机器人不会攻击，不会死亡，你不使用技能生成的功能，因为你只会生成防御技能，并且每回合结束都会回满血量"""
+    actor_training_robot.kick_off_message += f"""\n注意：你作为一个训练机器人只会最基本的攻击和防御技能，但是每个回合结束时都会回满血量。"""
 
     stage_cave5 = create_stage_cave5()
     stage_cave5.actors = [actor_training_robot]

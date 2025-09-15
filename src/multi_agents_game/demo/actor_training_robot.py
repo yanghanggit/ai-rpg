0e93ff3abe959274635ec4538ecf1692863e98bd
Hongyu Cai <145795296+TheBestDustman@users.noreply.github.com>
2025-09-15 15:08:05 +0800

    训练机器人无限HP测试

--- src/multi_agents_game/demo/actor_training_robot.py
+++ src/multi_agents_game/demo/actor_training_robot.py
@@ -23,7 +23,7 @@ def create_actor_training_robot() -> Actor:
         rpg_character_profile=RPGCharacterProfile(base_dexterity=1, base_max_hp=1000),
         type=ActorType.MONSTER,
         campaign_setting=FANTASY_WORLD_RPG_CAMPAIGN_SETTING,
-        actor_profile="""你是一个训练机器人，只会最基本的防御和攻击，你不会生成观察和利用环境的技能，但你是死不掉的，你有无限的血量，
+        actor_profile="""你是一个训练机器人，只会最基本的防御和攻击，你不会生成观察和利用环境的技能，但你是死不掉的，你有无限生命，回合结束时HP会自动恢复至最大值，
          【战斗循环】  
         1. 防御（格挡或减伤，等待敌人出手）。  
         2. 攻击（基础轻攻击或者重击）。

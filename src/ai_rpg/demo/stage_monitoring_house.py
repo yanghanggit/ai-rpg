f4b43e7d079786de22e55c7f0cfb09e8395a7e21
Hongyu Cai <145795296+TheBestDustman@users.noreply.github.com>
2025-12-03 10:54:03 +0800

    玩家角色和监视房间修改

--- src/ai_rpg/demo/stage_monitoring_house.py
+++ src/ai_rpg/demo/stage_monitoring_house.py
@@ -19,7 +19,7 @@ def create_stage_monitoring_house() -> Stage:
         kick_off_message="",
         campaign_setting=FANTASY_WORLD_RPG_CAMPAIGN_SETTING,
         type=StageType.HOME,
-        stage_profile="你是一个监视之屋，能看见新奥拉西斯城内外的各个地方",
+        stage_profile="你是一个监视之屋，房间里漆黑一片，唯独有一块屏幕能看见新奥拉西斯城内外的各个地方。只有玩家XYZ能进入这里，并通过屏幕穿梭于各个场景。",
         actors=[],
         global_game_mechanics=FANTASY_WORLD_RPG_GLOBAL_GAME_MECHANICS,
     )
